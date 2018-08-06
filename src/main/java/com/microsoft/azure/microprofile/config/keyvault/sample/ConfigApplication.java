5ea0090fe7517eeeaa2a0a89ff05bc7ee73d714a
Jonathan Giles <jonathan@jonathangiles.net>
2018-08-06 13:53:40 +1200

    Initial code push

---
+++ src/main/java/com/microsoft/azure/microprofile/config/keyvault/sample/ConfigApplication.java
@@ -0,0 +1,9 @@
+package com.microsoft.azure.microprofile.config.keyvault.sample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ConfigApplication extends Application {
+
+}
